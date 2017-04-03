f26932bced82ca3e3e051945e568fd9693bb609c
Emmanuel Sarella <emmanuel.r.sarella@questdiagnostics.com>
2017-04-02 21:06:04 -0700

    Initial commit

---
+++ Flicks/DetailViewController.swift
@@ -0,0 +1,66 @@
+//
+//  DetailViewController.swift
+//  Flicks
+//
+//  Created by Emmanuel Sarella on 3/29/17.
+//  Copyright © 2017 Emmanuel Sarella. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var infoView: UIView!
+    
+    var movie: NSDictionary!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width
+            , height: infoView.frame.origin.y + infoView.frame.size.height)
+
+        let title = movie["title"]  as? String
+        titleLabel.text = title
+        
+        let overview = movie["overview"]  as? String
+        overviewLabel.text = overview
+        
+        overviewLabel.sizeToFit()
+        
+        if let posterPath = movie["poster_path"] as? String {
+            let posterBaseUrl = "https://image.tmdb.org/t/p/w500"
+            let posterUrl = URL(string: posterBaseUrl + posterPath)
+            posterImageView.setImageWith(posterUrl!)
+        }
+        else {
+            // No poster image. Can either set to nil (no image) or a default movie poster image
+            // that you include as an asset
+            posterImageView = nil
+        }
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
